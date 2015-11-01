de77d7ea05716ada5521fcef5cb5136f6c4943a1
Arnaud Esteve <arnaud.esteve@gmail.com>
2015-11-01 01:23:43 +0100

    unused attribute

--- src/test/java/mock/controllers/CrudController.java
+++ src/test/java/mock/controllers/CrudController.java
@@ -8,7 +8,6 @@ import com.github.aesteve.nubes.hibernate.annotations.Create;
 import com.github.aesteve.nubes.hibernate.annotations.GetById;
 import com.github.aesteve.nubes.hibernate.annotations.QueryList;
 import com.github.aesteve.nubes.hibernate.queries.FindById;
-import com.github.aesteve.nubes.hibernate.services.HibernateService;
 import com.github.aesteve.vertx.nubes.annotations.Controller;
 import com.github.aesteve.vertx.nubes.annotations.mixins.ContentType;
 import com.github.aesteve.vertx.nubes.annotations.params.Param;
@@ -22,8 +21,6 @@ import mock.domains.Dog;
 @Controller("/api/dogs")
 @ContentType("application/json")
 public class CrudController {
-
-	HibernateService hibernate;
 	
 	@POST
 	@Create
